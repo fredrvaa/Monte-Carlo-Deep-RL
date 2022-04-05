eada0c065374ec1c91c25640be32903373f627d1
Fredrik <fredrvaa@stud.ntnu.no>
2022-04-05 10:30:07 +0200

    Add comment to fit script

--- fit.py
+++ fit.py
@@ -9,17 +9,19 @@ from learner.reinforcement_learner import ReinforcementLearner
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
+# Parse arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--config', help='path/to/fit/config', type=str)
 args = parser.parse_args()
 
+# Parse config
 configparser = FitParser(args.config)
 
 environment: Environment = configparser.get_environment()
-
 actor_kwargs = configparser.get_actor_kwargs()
 actor: Actor = Actor(input_size=environment.state_size, output_size=environment.n_actions, **actor_kwargs)
 
+# Run reinforcement learning
 rl = ReinforcementLearner(environment, actor)
 fit_kwargs = configparser.get_fit_kwargs()
 rl.fit(**fit_kwargs)
