d7bf5f0a995106bcfd7b6cbbaea8852c7211ae2b
Fredrik <fredrvaa@stud.ntnu.no>
2022-04-06 11:09:00 +0200

    Add league mode to oht script

--- oht.py
+++ oht.py
@@ -10,7 +10,12 @@ from oht_client.hex_client import HexClient
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-m', '--model', help='path/to/7x7/model', type=str)
+parser.add_argument('-l', '--league', action='store_true')
 args = parser.parse_args()
 
 client = HexClient(model_path=args.model)
-client.run()
+
+if args.league:
+    client.run(mode='league')
+else:
+    client.run()
